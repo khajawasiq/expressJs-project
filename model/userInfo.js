ceb0fe6aa8744c9da9b2aa72c2666dade33a9b23
khaja.wasiq <khajawasiq33.kw@gmail.com>
2024-09-09 02:34:44 +0530

    implement user  authentication user model

---
+++ model/userInfo.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userInfoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+export default mongoose.model("users",userInfoSchema)

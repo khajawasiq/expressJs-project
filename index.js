9333feebd9eef5203a7bcb075b2299e2120f33db
khaja.wasiq <khajawasiq33.kw@gmail.com>
2024-09-09 02:33:25 +0530

    implement user  authentication

--- index.js
+++ index.js
@@ -1,36 +1,83 @@
-import express from 'express'
-import path from 'path';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+import UserInfo from "./model/userInfo.js";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
 const app = express();
-const port = 4000
+const port = 4000;
+dotenv.config();
+const url = process.env.URL;
 
-app.use(express.static(path.join(path.resolve(),"public")))
-app.set("view engine","ejs");
-app.use(express.urlencoded({extended:true}))
-app.get('/', (req, res) => {
-    const pathLocation = path.resolve();
-    // res.sendFile(path.join(pathLocation, "/index.html"));
-    res.render("index",{name:"wasiq"})
+mongoose
+    .connect(url)
+    .then(() => {
+        console.log("Connected to MongoDB!");
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch((error) => console.log("Error connecting to MongoDB:", error));
+app.use(express.static(path.join(path.resolve(), "public")));
+app.set("view engine", "ejs");
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+const isAuthenticated = async(req, res, next) => {
+    console.log(req.cookies);
+    const { token } = req.cookies;
+    if (token) {
+    const decoded=jwt.verify(token,"ssadsad")
+    console.log("🚀 ~ isAuthenticated ~ decoded:", decoded);
+   req.user = await UserInfo.findById(decoded.id);
+console.log(await UserInfo.findById(decoded.id),"54",req.user);
+
+        next();
+    } else {
+        res.render("form");
+    }
+
+};
+
+
+
+app.get("/",isAuthenticated, (req, res) => {
+
+
+        console.log(req.user,"12");
+        res.render("logout",{name:req.user.name});
+
 });
-app.get('/login', async(req, res) => {
-    try {
-        res.render("form")
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-})
-app.get('/success', async(req, res) => {
-    try {
-        res.render("success")
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-})
-const users=[];
-app.post('/login', (req, res) => {
-     console.log(req.body);
-     users.push({userName:req.body.name,userEmail:req.body.email})
-    //  res.json(users)
-    res.redirect("/success")
-})
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+app.get("/success", async (req, res) => {
+    try {
+        res.render("success");
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+// app.get('/add', async(req, res) => {
+//    await UserInfo.create({name:"wasq",email:"wasiq@naivex.com"})
+//     res.send("add");
+// })
+app.get('/logout', (request, response) => {
+    response.cookie("token", null, {
+        httpOnly: true,
+        expires: new Date(Date.now()),
+    });
+    response.redirect("/");
+})
+app.post("/login", async (req, res) => {
+    const { name, email, password } = req.body;
+    const user= await UserInfo.create({ name, email, password });
+
+    const token=jwt.sign({id:user._id},"ssadsad")
+    res.cookie("token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 60 * 1000),
+    });
+    console.log(req.body);
+    //  res.json(users)
+    res.redirect("/");
+});
